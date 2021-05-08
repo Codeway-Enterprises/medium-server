1f72231a2fbe4e03159b52d693614708af8f7523
codeway-0841 <joincoin.codeway@gmail.com>
2021-05-08 06:03:06 +0500

    Post, User, UserProfile uchun TypeORM-da ob'ektni qayta yozishni yakunlanldi

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import { ApolloServer, gql } from 'apollo-server-koa';
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection, getConnection, getManager } from 'typeorm';
 import routes from './routes';
+import { Post } from './entity/Post';
 
 const app = new Koa();
 
-/* setup middlewares */
 app.use(bodyParser());
 app.use(routes.routes()).use(routes.allowedMethods());
 
-/* integrate GraphQL */
 const typeDefs = gql`
   type Query {
     medium: String
@@ -19,16 +18,21 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    medium: () => 'Medium-server',
+    medium: () => 'Happiness',
   },
 };
 
 const apollo = new ApolloServer({ typeDefs, resolvers });
 apollo.applyMiddleware({ app });
 
+/**
+ * initial tasks except Koa middlewares
+ */
 async function initialize() {
   try {
     await createConnection();
+    const posts = await getManager().createQueryBuilder(Post, 'post').getMany();
+
     console.log('Postgres connection is established');
   } catch (e) {
     console.log(e);
