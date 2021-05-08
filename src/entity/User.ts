cf46ee99a3631ce58e1006e6757ce5b831a6ee43
codeway-0841 <joincoin.codeway@gmail.com>
2021-05-08 05:52:05 +0500

    Typeorm yordamida birinchi model yaratildi

---
+++ src/entity/User.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Index,
+  Unique,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Index()
+  @Column({ unique: true, length: 255 })
+  username!: string;
+
+  @Index()
+  @Column({ unique: true, length: 255 })
+  email!: string;
+
+  @Column('timestampz')
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @Column('timestamptz')
+  @UpdateDateColumn()
+  updated_at!: Date;
+
+  @Column({ default: false })
+  is_certified!: boolean;
+}
